3e927e620a02a954fcc9fc71358ffb34d4dd7e5b
ugoswami11 <ugoswami1112@gmail.com>
2023-02-05 20:42:51 +0530

    password-reset-mail

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7ad134510ced2d0f3ab9c6041fdaa897'
@@ -12,4 +14,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.zoho.in'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USER')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASS')
+mail = Mail(app)
+
 from blog import routes
