13274a70d7a890085a3f1e29afa29d26b6f606cb
ugoswami11 <ugoswami1112@gmail.com>
2023-02-25 13:28:55 +0530

    debug-off-ready-for-deployment

--- blog/config.py
+++ blog/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.zoho.in'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('MAIL_USER')
-    MAIL_PASSWORD = os.getenv('MAIL_PASS')
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
